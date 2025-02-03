867ba8eaf30a0d7fff4190d948eb2abe31f1e2c7
SwayamS112 <soodswayam41@gmail.com>
2025-02-04 00:05:06 +0530

    use some props and basics

---
+++ first-Project/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Product from './Product'
+function App() {
+  return (
+    <>
+    <h1>Hello Everyone</h1>
+    <Product name="iphone 14" price={80000}/>
+    <Product name="samsung s23"price={60000}/>
+    <Product name="motorola"price={43095}/>
+    <Product name="redmi note 3"price={92540}/>
+ </>
+  )
+}
+
+export default App
---
+++ first-Project/src/Product.jsx
@@ -0,0 +1,10 @@
+// export default function Product()
+function Product({name,price}){
+    return(
+        <>
+        <h3>Product name: {name} and 
+            its price is {price}</h3>
+        </>
+    )
+}
+export default Product
